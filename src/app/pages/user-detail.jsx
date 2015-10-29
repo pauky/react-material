7fc00c977c3993c467e600b3e3dffb7cf0cea624
pauky <glowrypauky@gmail.com>
2015-10-29 23:33:53 +0800

    user-detail fix

--- src/app/pages/user-detail.jsx
+++ src/app/pages/user-detail.jsx
@@ -21,8 +21,12 @@ class PartyDetail extends React.Component {
   render() {
     return (
       <Layout>
-        <div className="user-detail-page">
+        <div className="user-detail-page text-center">
           <Avatar src="http://material-ui.com/images/chexee-128.jpg" className="avatar" />
+          <p className="username">Pauky</p>
+          <p className="school">南方医科大学</p>
+          <p className="word">北京品途天下科技有限公司-广研所-web前端开发工程师</p>
+          <p className="description">我是一只IT小小鸟，不知疲倦飞呀飞...</p>
         </div>
       </Layout>
     );
