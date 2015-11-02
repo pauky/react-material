3bb77830708651811e5dc43a28b5f9b68a71ab4a
pauky <yangruiwei@pintu360.com>
2015-11-02 12:18:24 +0800

    launch-party fix

--- src/app/components/layout.jsx
+++ src/app/components/layout.jsx
@@ -6,8 +6,39 @@
 const React = require('react');
 const AppTopBar = require('./app-top-bar');
 const AppFooter = require('./app-footer');
+const {
+  Styles,
+  } = require('material-ui');
+const ThemeManager = Styles.ThemeManager;
+const DefaultRawTheme = Styles.LightRawTheme;
 
 const Layout = React.createClass({
+
+  contextTypes : {
+    muiTheme: React.PropTypes.object,
+  },
+
+  //for passing default theme context to children
+  childContextTypes: {
+    muiTheme: React.PropTypes.object,
+  },
+
+  getChildContext () {
+    return {
+      muiTheme: this.state.muiTheme,
+    };
+  },
+
+  getInitialState () {
+    return {
+      muiTheme: this.context.muiTheme ? this.context.muiTheme : ThemeManager.getMuiTheme(DefaultRawTheme),
+    };
+  },
+
+  componentWillReceiveProps (nextProps, nextContext) {
+    let newMuiTheme = nextContext.muiTheme ? nextContext.muiTheme : this.state.muiTheme;
+    this.setState({muiTheme: newMuiTheme});
+  },
 
   render() {
 
--- src/app/pages/launch-party.jsx
+++ src/app/pages/launch-party.jsx
@@ -4,8 +4,9 @@
  */
 
 const React = require('react');
-let { TextField, RaisedButton } = require('material-ui');
+let { TextField, RaisedButton, DatePicker, TimePicker, Styles } = require('material-ui');
 const Layout = require('app/components/layout');
+require('style/pages/launch-party.less');
 
 class LaunchParty extends React.Component {
 
@@ -17,18 +18,42 @@ class LaunchParty extends React.Component {
     return (
       <Layout>
         <div className="launch-party-page text-center">
-          <TextField
-            hintText="填写聚会名"
-            floatingLabelText="聚会名" />
-          <TextField
-            className="description text-left"
-            hintText="填写聚会简介"
-            floatingLabelText="聚会简介"
-            multiLine={true} />
-          <TextField
-            hintText="人数（不包括自己）"
-            floatingLabelText="人数（不包括自己）"/>
           <div>
+            <TextField
+              hintText="填写聚会名"
+              floatingLabelText="聚会名" />
+          </div>
+          <div>
+            <TextField
+              className="description text-left"
+              hintText="填写聚会简介"
+              floatingLabelText="聚会简介"
+              multiLine={true} />
+          </div>
+          <div>
+            <TextField
+              hintText="聚会地址"
+              floatingLabelText="聚会地址"/>
+          </div>
+          <div className="party-date">
+            <DatePicker
+              hintText="聚会日期" />
+          </div>
+
+          <div>
+            <TimePicker
+              ref="picker24hr"
+              format="24hr"
+              hintText="聚会具体时间"
+              autoOk={true} />
+          </div>
+
+          <div>
+            <TextField
+              hintText="人数（不包括自己）"
+              floatingLabelText="人数（不包括自己）"/>
+          </div>
+          <div className="action-btns">
             <RaisedButton label="发布聚会" secondary={true} />
           </div>
         </div>
